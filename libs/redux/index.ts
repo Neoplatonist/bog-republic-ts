c16791a74db908eb1dc93d835763ab60ca82c315
Neoplatonist <mr.neoplatonist@gmail.com>
2022-08-23 17:08:52 +0900

    feat: setup and configured a default redux store with thunk

---
+++ libs/redux/index.ts
@@ -0,0 +1,46 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import {
+  AnyAction,
+  configureStore,
+  ConfigureStoreOptions,
+  ThunkDispatch,
+} from '@reduxjs/toolkit';
+import type { Store } from '@reduxjs/toolkit';
+import { Context, createWrapper } from 'next-redux-wrapper';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+export const createStore: () => Store = (
+  options?: ConfigureStoreOptions['preloadedState'] | undefined
+) =>
+  configureStore({
+    reducer: {},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+    devTools: isDev,
+    ...options,
+  });
+
+// create store to make the
+// dispatch, thunk dispatch, and useSelector types avaliable
+const store = createStore();
+
+// create a makeStore function for next-redux-wrapper
+// eslint-disable-next-line no-unused-vars
+export const makeStore = (context: Context) => store;
+
+// assembled next-redux-wrapper
+export const wrapper = createWrapper(makeStore, {
+  debug: isDev,
+});
+
+// Selector with type
+export type RootState = ReturnType<typeof store.getState>;
+export const useTypedSelector: TypedUseSelectorHook<any> = useSelector;
+
+// Dispatch with type
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+
+// ThunkDispatch with Type
+export type AppThunkDispatch = ThunkDispatch<RootState, any, AnyAction>;
+export const useAppThunkDispatch: () => AppThunkDispatch = useDispatch;
