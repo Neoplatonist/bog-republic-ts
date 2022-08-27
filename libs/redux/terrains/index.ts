5b86cb41dbf2aa8fa4a68d028610ac89eb4d84e5
Neoplatonist <mr.neoplatonist@gmail.com>
2022-08-27 20:54:49 +0900

    feat: added rehydration to the terrains reducer

--- libs/redux/terrains/index.ts
+++ libs/redux/terrains/index.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { HYDRATE } from 'next-redux-wrapper';
+import { diff } from 'jsondiffpatch';
+import type { RootState } from '@/libs/redux';
 import { z } from 'zod';
 import { TerrainObjectListSchema } from '@/libs/types';
-import type { RootState } from '@/libs/redux';
 
 const TerrainStateSchema = z.object({
   data: TerrainObjectListSchema,
@@ -23,6 +25,28 @@ const slice = createSlice({
     setTerrains: (state, { payload }) => {
       state.data = [...payload];
       state.error = null;
+    },
+  },
+  extraReducers: {
+    // https://github.com/kirill-konshin/next-redux-wrapper#how-it-works
+    // This allows for rehydration of the store from the server
+    // while on the client side.
+    // This is needed for SSR and SSG.
+    [HYDRATE]: (state: RootState, action: PayloadAction<any>) => {
+      const stateDiff = diff(state, action.payload);
+      const wasBumpedOnClient = stateDiff?.terrains?.data;
+
+      // keep existing state or use hydrated state
+      const data = wasBumpedOnClient ? state.terrains : action.payload.terrains;
+
+      const newState = {
+        ...state.terrains,
+        ...action.payload.terrains,
+        // keep existing state or use hydrated
+        ...data,
+      };
+
+      return newState;
     },
   },
 });
