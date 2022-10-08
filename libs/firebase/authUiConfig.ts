95b153093501d1c20f33b692e88e01585c25bf9d
Neoplatonist <mr.neoplatonist@gmail.com>
2022-10-09 01:17:11 +0900

    refactor: updated authUiConfig to match the server signin and login of users; removed axios

--- libs/firebase/authUiConfig.ts
+++ libs/firebase/authUiConfig.ts
@@ -1,8 +1,3 @@
-/**
- * This file is a work in progress.
- */
-
-import axios from '@/libs/axios';
 import { AuthProvider } from 'firebase/auth';
 
 /**
@@ -18,47 +13,6 @@ const uiConfig = (providers: AuthProvider[]) => ({
   tosUrl: '/terms-of-service',
   privacyPolicyUrl: '/privacy-policy',
   signInOptions: providers.map((provider) => provider.providerId),
-  callbacks: {
-    // eslint-disable-next-line no-unused-vars
-    signInSuccessWithAuthResult: (authResult: any, redirectUrl?: any) => {
-      // eslint-disable-next-line no-unused-vars
-      const { user, credential } = authResult;
-      // eslint-disable-next-line prefer-destructuring
-      const isNewUser = authResult.additionalUserInfo.isNewUser;
-
-      // use .then solution instead of async/await
-      user.getIdToken().then((idToken: string) => {
-        if (isNewUser) {
-          // is a new user, creates new user on server
-          axios.post(
-            '/user',
-            {},
-            {
-              headers: {
-                Authorization: `Bearer ${idToken}`,
-              },
-            }
-          );
-        } else {
-          // not a new user, just updated the last login timestamp
-          // might not be needed
-          axios.post(
-            '/user/login',
-            {},
-            {
-              headers: {
-                Authorization: `Bearer ${idToken}`,
-              },
-            }
-          );
-        }
-      });
-
-      if (!isNewUser) return true;
-
-      return false;
-    },
-  },
 });
 
 export default uiConfig;
