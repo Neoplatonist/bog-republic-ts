fc1fb2ecbce33839c7f80feaf4d9cb2339f2740b
Neoplatonist <16985758+Neoplatonist@users.noreply.github.com>
2023-04-10 16:19:20 +0900

    Feat/design/colors (#17)
    * feat: closes gh-10 added userGuardians api to redux
    
    * feat: closes gh-8 added userTerrains typ
    
    * fix: updated userTerrains type to match the server
    
    * fix: added custom color scheme to daisyUI; makes the color contrast WCAG compliant
    
    ---------
    
    Co-authored-by: tofusupreme <yunishbacon@gmail.com>
    Co-authored-by: Neoplatonist <Neoplatonist@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,28 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-   // eslint-disable-next-line global-require
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#ADC178",
+          "secondary": "#DDE5B6",
+          "accent": "#A98467",
+          "neutral": "#6C584C",
+          "base-100": "#F0EAD2",
+          "info": "#F6BD60",
+          "success": "#B3B394",
+          "warning": "#EE7444",
+          "error": "#EC4946",
+        },
+      },
+    ],
+  },
+  // eslint-disable-next-line global-require
   plugins: [require("daisyui")],
 };
