edad3b0b9cdf0d24c5dadfa693b5d12da4905a32
Neoplatonist <16985758+Neoplatonist@users.noreply.github.com>
2023-04-10 16:58:17 +0900

    fix: added a styled header to layout/game with a stateful user and logout button

--- layout/game.tsx
+++ layout/game.tsx
@@ -1,14 +1,52 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, MouseEvent, ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { getAuth, signOut } from 'firebase/auth';
+import { useTypedSelector } from '@/libs/redux';
+import { selectUser } from '@/libs/redux/user';
 
 type Props = {
   children: ReactNode;
 };
 
-// eslint-disable-next-line arrow-body-style
 const GameLayout: FC<Props> = ({ children }) => {
+  const router = useRouter();
+
+  // Redux Selectors
+  const user = useTypedSelector(selectUser);
+
+  // ---------------------------------- User Actions ----------------------------------
+  // Logs the user out
+  const handleLogout = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    signOut(getAuth())
+      .then(() => router.push('/')); // Redirects to the home page
+  };
+
   return (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
+      {/* Header */}
+      <header className="navbar bg-success">
+        <div className='flex-1'>
+          <Link href="/game">
+            <a className='btn btn-ghost normal-case text-xl'>Bog Republic</a>
+          </Link>
+        </div>
+
+        {/* User Stats Card */}
+        <div className='flex-none'>
+          <p>{user && user.username} the Frog</p>
+
+          <button
+            type='button'
+            className='btn btn-square btn-ghost'
+            onClick={handleLogout}
+          >
+            X
+          </button>
+        </div>
+      </header>
+
       {children}
     </>
   );
