77535d5e195875f390e68e22affe116ac3ff6a58
Neoplatonist <16985758+Neoplatonist@users.noreply.github.com>
2023-04-20 16:42:30 +0900

    feat: calling userTerrainsApi endpoint pushes the results in to the slice state now

--- libs/redux/userTerrains/index.ts
+++ libs/redux/userTerrains/index.ts
@@ -3,7 +3,8 @@ import { HYDRATE } from 'next-redux-wrapper';
 import { diff } from 'jsondiffpatch';
 import { z } from 'zod';
 import type { RootState } from '@/libs/redux';
-import { UserTerrainsObjectListSchema } from '@/libs/types';
+import { UserTerrainsObject, UserTerrainsObjectListSchema } from '@/libs/types';
+import userTerrainsApi from '@/libs/redux/userTerrains/api';
 
 const UserTerrainStateSchema = z.object({
   data: UserTerrainsObjectListSchema,
@@ -22,27 +23,40 @@ const slice = createSlice({
   initialState,
   reducers: {
   },
-  extraReducers: {
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      userTerrainsApi.endpoints.getUserTerrains.matchFulfilled,
+      (state, { payload }) => ({
+        ...state,
+        data: payload.map((terrain: UserTerrainsObject) => ({
+          ...terrain,
+          isContributionLocked: false,
+        }))
+      })
+    );
+
     // https://github.com/kirill-konshin/next-redux-wrapper#how-it-works
     // This allows for rehydration of the store from the server
     // while on the client side.
     // This is needed for SSR and SSG.
-    [HYDRATE]: (state: RootState, action: PayloadAction<any>) => {
-      const stateDiff = diff(state, action.payload);
-      const wasBumpedOnClient = stateDiff?.userTerrains?.data;
+    return {
+      [HYDRATE]: (state: RootState, action: PayloadAction<any>) => {
+        const stateDiff = diff(state, action.payload);
+        const wasBumpedOnClient = stateDiff?.userTerrains?.data;
 
-      // keep existing state or use hydrated state
-      const data = wasBumpedOnClient ? state.userTerrains : action.payload.userTerrains;
+        // keep existing state or use hydrated state
+        const data = wasBumpedOnClient ? state.userTerrains : action.payload.userTerrains;
 
-      const newState = {
-        ...state.userTerrains,
-        ...action.payload.userTerrains,
-        // keep existing state or use hydrated
-        ...data,
-      };
+        const newState = {
+          ...state.userTerrains,
+          ...action.payload.userTerrains,
+          // keep existing state or use hydrated
+          ...data,
+        };
 
-      return newState;
-    },
+        return newState;
+      },
+    };
   },
 });
 
