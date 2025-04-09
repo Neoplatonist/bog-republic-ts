55696054dfc1be850fcbc288e168fc0c528c4aea
Neoplatonist <16985758+Neoplatonist@users.noreply.github.com>
2025-04-09 14:38:28 +0900

    fix: refactored and renamed, it now properly rehydrates data, and is more mobile oriented

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,67 +1,81 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { useUser } from '@/libs/contexts/UserProvider';
 import { useGetTerrainsQuery } from '@/libs/redux/terrains/api';
 import { useGetUserTerrainsQuery } from '@/libs/redux/userTerrains/api';
+import { hydrateUser } from '@/libs/redux/user';
 import { hydrateTerrains } from '@/libs/redux/terrains';
 import { hydrateUserTerrains } from '@/libs/redux/userTerrains';
+// import UserCard from '@/components/UserCard';
+import { useGetUserQuery } from '@/libs/redux/user/api';
+import DashboardContent from './content';
 
 const DashboardPage = () => {
   const dispatch = useDispatch();
-  const user = useUser();
+  const hankoUser = useUser();
 
+  const hasHydratedUser = useRef(false);
+  const hasHydratedTerrains = useRef(false);
+  const hasHydratedUserTerrains = useRef(false);
+
+  const {
+    data: user,
+    // isLoading: userLoading,
+    // isSuccess: isUserSuccess,
+    // isFetching: isUserFetching,
+    // isUninitialized: isUserUninitialized,
+    currentData: userCurrentData,
+  } = useGetUserQuery('', {
+    refetchOnMountOrArgChange: false,
+  });
   const {
     data: terrains,
     isLoading: terrainsLoading,
-    isSuccess: isTerrainsSuccess,
+    // isSuccess: isTerrainsSuccess,
     // isFetching: isTerrainsFetching,
     // isUninitialized: isTerrainsUninitialized,
-    // currentData: terrainsCurrentData,
+    currentData: terrainsCurrentData,
   } = useGetTerrainsQuery(undefined, {
     refetchOnMountOrArgChange: false,
   });
   const {
     data: userTerrains,
     isLoading: userTerrainsLoading,
-    isSuccess: isUserTerrainsSuccess,
+    // isSuccess: isUserTerrainsSuccess,
     // isFetching: isUserTerrainsFetching,
     // isUninitialized: isUserTerrainsUninitialized,
-    // currentData: userTerrainsCurrentData,
-  } = useGetUserTerrainsQuery('', {
-    refetchOnMountOrArgChange: false,
-  });
-  // console.log({
-  //   terrains: {
-  //     data: terrains,
-  //     loading: terrainsLoading,
-  //     fetching: isTerrainsFetching,
-  //     uninitialized: isTerrainsUninitialized,
-  //     currentData: terrainsCurrentData,
-  //   },
-  //   userTerrains: {
-  //     data: userTerrains,
-  //     loading: userTerrainsLoading,
-  //     fetching: isUserTerrainsFetching,
-  //     uninitialized: isUserTerrainsUninitialized,
-  //     currentData: userTerrainsCurrentData,
-  //   },
-  // });
+    currentData: userTerrainsCurrentData,
+  } = useGetUserTerrainsQuery(undefined);
 
   useEffect(() => {
-    if (isTerrainsSuccess && terrains) {
-      dispatch(hydrateTerrains({ data: terrains, errors: null }));
+    const userData = user || userCurrentData;
+    console.log({ userData });
+    if (userData && !hasHydratedUser.current) {
+      dispatch(hydrateUser({ data: userData, errors: null }));
+      hasHydratedUser.current = true;
     }
-  }, [dispatch, isTerrainsSuccess, terrains]);
+  }, [dispatch, user, userCurrentData]);
 
   useEffect(() => {
-    if (isUserTerrainsSuccess && userTerrains) {
-      dispatch(hydrateUserTerrains({ data: userTerrains, errors: null }));
+    const terrainsData = terrains || terrainsCurrentData;
+    if (terrainsData && !hasHydratedTerrains.current) {
+      dispatch(hydrateTerrains({ data: terrainsData, errors: null }));
+      hasHydratedTerrains.current = true;
     }
-  }, [dispatch, isUserTerrainsSuccess, userTerrains]);
+  }, [dispatch, terrains, terrainsCurrentData]);
 
-  if (!user) {
+  useEffect(() => {
+    const userTerrainsData = userTerrains || userTerrainsCurrentData;
+    console.log({ userTerrainsData });
+    if (userTerrainsData && !hasHydratedUserTerrains.current) {
+      dispatch(hydrateUserTerrains({ data: userTerrainsData, errors: null }));
+      hasHydratedUserTerrains.current = true;
+    }
+  }, [dispatch, userTerrains, userTerrainsCurrentData]);
+
+  if (!hankoUser) {
     return <div>Not logged in</div>;
   }
 
@@ -91,10 +105,12 @@ const DashboardPage = () => {
   }
 
   return (
-    <div className="p-24">
-      <div>User id: {user.id}</div>
+    <div className="sm:p-5 xl:p-24">
+      {/* <div>User id: {user.id}</div>
       <div>User email: {user.email}</div>
-      <div>User Name: {user.username}</div>
+      <div>User Name: {user.username}</div> */}
+
+      <DashboardContent />
     </div>
   );
 };
